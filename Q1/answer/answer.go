c364dbdfd491c179e383b28a716a3ed011e1dbac
Burak Akkaya <burkakkaya8@gmail.com>
2022-11-29 19:34:16 +0300

    q1 refactors

--- Q1/answer/answer.go
+++ Q1/answer/answer.go
@@ -7,13 +7,19 @@ import (
 	"unicode/utf8"
 )
 
-func SortByCharacter(input []string, char string) []string {
+/*
+Sorts the given string array by the given string.
+@param : sortedBy - string to be sorted by.
+@param : input - []string to be sorted.
+*/
+func SortByCharacter(input []string, sortedBy string) []string {
 	sort.Slice(input, func(i, j int) bool {
 		s1, s2 := input[i], input[j]
-		count1, count2 := strings.Count(s1, char), strings.Count(s2, char)
+		count1, count2 := strings.Count(s1, sortedBy), strings.Count(s2, sortedBy)
 		if count1 != count2 {
 			return count1 > count2
 		}
+		//len() returns the UTF-8 encoded byte length, not the number of runes. For the letter, use utf8.RuneCountInString()
 		return utf8.RuneCountInString(s1) > utf8.RuneCountInString(s2)
 	})
 
--- Q1/main.go
+++ Q1/main.go
@@ -1,9 +1,4 @@
 package main
 
-import (
-	answer "byFood/Q1/answer"
-)
-
 func main() {
-	answer.SortByCharacter([]string{"aaaasd", "a", "aab", "aaabcd", "ef", "cssssssd", "fdz", "kf", "zc", "lklklklklklklklkl", "l"}, "a")
 }
